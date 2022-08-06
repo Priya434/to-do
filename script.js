5d7eefdeb5efc658e5145a43cccd193d0f59f4ca
Priyanshu Kumawat <priyanshukumawat434@gmail.com>
2022-08-06 17:53:40 +0530

    Add Js logic to add list item

--- script.js
+++ script.js
@@ -2,4 +2,42 @@
 
 // global constants
 const input = document.querySelector("#input_text");
-const add = document.querySelector("#add_btn");
+const addBtn = document.querySelector("#add_btn");
+const list = document.querySelector("#list");
+
+// event listener
+addBtn.addEventListener("click", addItem);
+window.addEventListener("keydown", eventCheck);
+
+// function for event listener
+function eventCheck(event) {
+    if (event.type = "keydown") {
+        if (event.key === "Enter") addItem();
+    }
+}
+
+// function to check if input is empty
+function addItem() {
+    const inputTxt = document.querySelector("#input_text");
+
+    if (inputTxt.value !== "") addListItem(inputTxt.value);
+
+    inputTxt.value = "";
+    inputTxt.focus();
+}
+
+// function to add list item
+function addListItem(text) {
+    const listItem = document.createElement("li");
+    const listItemTxt = document.createTextNode(text);
+    listItem.appendChild(listItemTxt);
+
+    const list = document.querySelector("#list");
+    list.appendChild(listItem);
+}
+
+// on opening window
+window.onload = () => {
+    const inputTxt = document.querySelector("#input_text");
+    // inputTxt.focus();
+};
